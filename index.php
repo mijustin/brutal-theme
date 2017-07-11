36d1fcd28209ecace270b316c1f4136163691fb7
Justin Jackson <justin@nerdnorth.com>
2017-07-10 17:08:09 -0700

    New index

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php get_header(); ?>
 
 <article>
+
 		<?php
-		if ( have_posts() ) : while ( have_posts() ) : the_post();
+		if ( have_posts() ) :
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
 
-		get_template_part( 'content', get_post_format() );
+			<?php
+			endif;
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+			endwhile;
+			the_posts_navigation();
+		else :
+			get_template_part( 'content', 'none' );
+		endif; ?>
 
-		endwhile; endif;
-		?>
-</article>
+	</article><!-- #primary -->
 
 <?php get_footer(); ?>
