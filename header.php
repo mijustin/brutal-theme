e55a9578c82725e52429858c9791de965007c2cc
Justin Jackson <justin@nerdnorth.com>
2017-07-15 09:59:57 -0700

    Trying Ryan Cowles solution
    http://ryanscowles.com/2013/01/set-wordpress-featured-image-thumbnail/

--- header.php
+++ header.php
@@ -5,12 +5,18 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta property="og:title" content="<?php echo the_title(); ?>"/>
-	<meta property="og:description" content="<?php echo $excerpt; ?>"/>
-	<meta property="og:type" content="article">
-	<meta property="og:url" content="<?php echo the_permalink(); ?>"/>
-	<meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
-	<meta property="og:image" content="<?php echo unfurl_og_image($img_src); ?>"/>
+
+	<meta property="og:title" content="<?php the_title(); ?>"/>
+	<meta property="og:description" content="<?php echo strip_tags(get_the_excerpt($post->ID)); ?>" />
+	<meta property="og:url" content="<?php the_permalink(); ?>"/>
+	<?php $fb_image = wp_get_attachment_image_src(get_post_thumbnail_id( get_the_ID() ), 'thumbnail'); ?>
+	<?php if ($fb_image) : ?>
+		<meta property="og:image" content="<?php echo $fb_image[0]; ?>" />
+	<?php endif; ?>
+	<meta property="og:type" content="<?php
+		if (is_single() || is_page()) { echo "article"; } else { echo "website";} ?>"
+	/>
+	<meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
 
 	<meta name="twitter:card" content="summary">
 	<meta name="twitter:site" content=“@mijustin">
