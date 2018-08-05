62e1db72b56cc06dd156b94339f74f4b42cfae9d
Mike Walker <mike@incredimike.com>
2018-08-04 20:22:46 -0700

    Added missing </head> tag; adjusted formatting

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>
-	  <?php if (is_home () ) { bloginfo('name'); echo ' | '; bloginfo('description'); } elseif ( is_category() ) { single_cat_title();  } elseif (is_single() ) { single_post_title(); } elseif (is_page() ) { single_post_title(); } else { wp_title('',true); } ?>
+	  	<?php if (is_home () ) { bloginfo('name'); echo ' | '; bloginfo('description'); } elseif ( is_category() ) { single_cat_title();  } elseif (is_single() ) { single_post_title(); } elseif (is_page() ) { single_post_title(); } else { wp_title('',true); } ?>
 	</title>
 	<link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo_rss('name'); ?>" href="<?php bloginfo_rss('atom_url') ?>">
@@ -15,13 +15,12 @@
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,700,700italic,100,100italic,300,300italic,500,400italic,500italic' rel='stylesheet' type='text/css'>
 	<?php wp_head();?>
-
-	<body <?php body_class(); ?>>
+</head>
+	
+<body <?php body_class(); ?>>
 	<?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
 	<header id="top" class="clearfix">
-	<?php if ( !(is_single() || is_page()) ) { ?>
-	  <h1>
-	    <?php bloginfo('name'); ?>
-	  </h1>
-  <?php } ?>
+	<?php if ( !(is_single() || is_page()) ) : ?>
+	  	<h1><?php bloginfo('name'); ?></h1>
+	<?php endif; ?>
 	</header>
